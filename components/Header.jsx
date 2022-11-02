942a9ca4b036aa47ad4f202cff57ef57e294d022
Sadiq <seeker99157@gmail.com>
2022-11-02 17:54:30 +0100

    build header section

--- components/Header.jsx
+++ components/Header.jsx
@@ -2,22 +2,28 @@ import { Button } from "@material-tailwind/react"
 
 function Header() {
     return (
-        <div className="flex items-center">
+        <div className="flex items-center sticky top-0 z-50 px-4 py-5 shadow-md bg-white">
             <Button
                 color="white"
                 buttonType="outline"
                 rounded={true}
-                iconOnly={true}
                 ripple={"dark"}
-                className="hidden md:inline-flex border-0 w-5 h-10 justify-center items-center"
+                className="inline-flex border-0 w-5 h-10 justify-center  items-center"
             >
-                <span className="material-icons text-2xl text-blue-600">
+                <span className="material-icons text-2xl text-gray-600">
                     menu
                 </span>
             </Button>
-            <h1 className="material-icons text-3xl text-blue-600 hidden md:inline-flex ml-2">
+            <span className="material-icons text-3xl text-blue-600  md:inline-flex ml-2">
                 description
-            </h1>
+            </span>
+
+            <div className="mx-5 md:mx-20 flex flex-grow items-center px-5 py-2 bg-gray-100 text-gray-600 rounded-lg focus-within:text-gray-700 focus-within:shadow-md" >
+                <span className="material-icons text-2xl text-gray-800">
+                    search
+                </span>
+                <input type="text" placeholder="Search" className="px-5 text-base bg-transparent outline-none flex-grow" />
+            </div>
         </div>
     )
 }
